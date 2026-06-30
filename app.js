32e5a579e87b136aab1d53ff8cc9bbcec6a480d7
Alexandra LU <60328714+alexandralu2@users.noreply.github.com>
2026-06-30 13:08:50 +0200

    Update app.js web

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function calcul(a, b) {
+/*function calcul(a, b) {
   var inutilisee = 0;
   if (a > 10) {
     return a + b;
@@ -8,3 +8,30 @@ function calcul(a, b) {
 }
 
 module.exports = calcul;
+*/
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+function calcul(a, b) {
+  return a + b;
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello DevOps - webapp sur Kubernetes ! 2 + 3 = ' + calcul(2, 3));
+});
+
+// Sonde utilisée par Kubernetes (readiness / liveness)
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+// On ne démarre le serveur que si le fichier est lancé directement
+// (évite de bloquer les tests Jest qui importent le module)
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log('Serveur démarré sur le port ' + PORT);
+  });
+}
+
+module.exports = { calcul, app };
